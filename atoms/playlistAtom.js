4da7d71feb5cef0a9ec1519a46c34f7b524ab06f
Alassane Fofana <74676173+Las07@users.noreply.github.com>
2022-05-27 08:04:50 +0200

    .

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const playlistState = atom({
+  key: "playlistState", // unique ID (with respect to other atoms/selectors)
+  default: null, // default value (aka initial value)
+});
+
+export const playlistIdState = atom({
+  key: "playlistIdState", // unique ID (with respect to other atoms/selectors)
+  default: "6rlk7KFtfUo1QWsPRSL9CZ", // default value (aka initial value)
+});
